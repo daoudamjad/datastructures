0924d1e858a7573e9a245f2bbf888e6a62b3f140
Andric Villanueva <avillanueva@andric-villanuevas-macbook-pro.local>
2012-12-11 19:30:25 -0500

    Added the needed stuff for arrays.

---
+++ arrays/src/main/java/lds/arrays/Array.java
@@ -0,0 +1,16 @@
+package lds.arrays;
+
+/**
+ *
+ * @author avillanueva
+ */
+public class Array {
+  // Use this only for quick tests. 
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  
+  public boolean doNothing() {
+    return true;
+  }
+}
